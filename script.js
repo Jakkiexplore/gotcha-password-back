22a0bb9a957657d073eebe7a2cda98e9ad730120
Jakkiexplore <yangjakki@gmail.com>
2024-01-12 03:46:33 -0600

    finished code.

--- script.js
+++ script.js
@@ -10,8 +10,28 @@ upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 numeric: "0123456789",
 special: "!@#$%^&*()_-+=?{}[]:;"
 }
+var getRandom = function(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 var generatePassword = function() {
+  var length = window.prompt("What length would you like your password to be?\n Enter a number of digits between " +
+    range.minLength + "-" + range.maxLength + ".");
+  if (length === null)
+    return "";
+}
+
+   // Have enter a vaild number between 8-128 //
+  length = parseInt(length);
+  if (Number.isNaN(length) || length < range.minLength || length > range.maxLength) {
+    window.alert("Enter a number between " + range.minLength + "-" + range.maxLength + ".");
+    return generatePassword();
+  }
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
